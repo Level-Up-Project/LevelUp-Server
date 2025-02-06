09c15304ea85f2384c8b6e18523941bbf8106698
Eshita-10 <eshitasingh72@gmail.com>
2025-02-06 17:10:47 +0530

    student model

--- src/models/student.model.ts
+++ src/models/student.model.ts
@@ -1,36 +1,143 @@
-import mongoose, { Document, Schema } from 'mongoose';
-
-// Defining the interface for the Student  document
-export interface IStudent extends Document {
-  studentCode: string;  // student code of user
-  subRole: string;  // Sub-role ( e.g., 'alumni', 'senior alumni', 'student','contributor')
-  enrolledCourses: mongoose.Types.ObjectId[];  // Array of courses previously enrolled in
-  currentCourse: mongoose.Types.ObjectId[];  // Array of courses currently enrolled in
-  bookedSessions: mongoose.Types.ObjectId[];  // Array of session  references (sessions booked by the user)
-  skills: string[];  // List of skills the user has
-  educationDetails:[{}] //array of object with year,degree and college name
-  pastExperience:[{}]  //array of object with company name , role, year
-}
+import mongoose, { Schema } from 'mongoose';
 
 
-// Define the schema for the Student model
+// Defining the interface for the education details
+interface educationDetailsSchema {
+    collegeName:string,
+    degree: string;
+    startYear:number,
+    endYear:number,
+    isPursuing:boolean
+   
+  }
+  
+
+  // Education details schema 
+  const educationDetailsSchema = new Schema({
+    collegeName: {
+      type: String,
+      required: [true, 'Please provide the college name'],
+      default: null,
+    },
+    degree: {
+      type: String,
+      required: [true, 'Please provide the degree'],
+      default: null,
+    },
+    startYear: {
+        type: Number,
+        required: [true, 'Please provide the start year'],
+        default: null,
+      },
+    endYear: {
+        type: Number,
+        required: [true, 'Please provide the end year'],
+        default: null,
+      },
+    isPursuing:{
+        type:Boolean,
+        required: true,
+        default:false,
+    },
+  });
+
+
+  // Defining the interface for past experience
+  interface pastExperienceSchema {
+    companyName:string,
+    startYear:number,
+    endYear:number,
+    role: string;
+    isWorking:boolean,
+  }
+  
+
+
+// Past experience schema definition
+  const pastExperienceSchema = new Schema({
+    companyName: {
+      type: String,
+      required: [true, 'Please provide the company name'], 
+      default: null,
+    },
+    role: {
+      type: String,
+      required: [true, 'Please provide the role'],
+      default: null,
+    },
+    startYear: {
+        type: Number,
+        required: [true, 'Please provide the start year'],
+        default: null,
+      },
+    endYear: {
+        type: String,
+        required: [true, 'Please provide the end year'],
+        default: null,
+      },
+    isWorking:{
+        type:Boolean,
+        required: true,
+        default:false
+    }
+  });
+
+  // Student schema definition
 const StudentSchema: Schema = new Schema(
   {
-    studentCode: {  type: String, required: true , unique:true,},
-    subRole: {type: String,required: true,},           //Sub-role ( e.g., 'alumni', 'senior alumni', 'student','contributor')
-    enrolledCourses: { type: [mongoose.Schema.Types.ObjectId],default: [] },
-    currentCourse:{ type: [mongoose.Schema.Types.ObjectId],default: [] },
-    bookedSessions: { type: [mongoose.Schema.Types.ObjectId], ref: 'Session', default: [] },  // Array of booked sessions
-   skills: { type: [String], default: [] },  // List of skills the user has
+    studentCode: {
+      type: String,
+      required: [true, 'Please provide your student code'],
+      unique:true,
+    },
+    subRole: {
+        type: String,
+        default: 'student',
+        enum: [ 'student', 'alumni', 'senior alumni', 'contributor'],
+        
+      },
+    enrolledCourses: { 
+        type: [mongoose.Schema.Types.ObjectId],
+        default: [], 
+        ref: 'Course', // refers to  Course model
+    },
+    currentCourse:{ 
+        type: [mongoose.Schema.Types.ObjectId],
+        default: [],
+        ref: 'Course', // refers to Course model
+    },
+    bookedSessions: { 
+        type: [mongoose.Schema.Types.ObjectId], 
+        ref: 'Session', //refers to session model
+        default: [] 
+    },
+    skills: { 
+        type: [String], 
+        default: [] 
+    }, 
+    educationDetails:[educationDetailsSchema],
+    pastExperience:[pastExperienceSchema],
 
-  
-   
-  
-  },
-  { timestamps: true }  // Automatically adds createdAt and updatedAt fields
+},
+  { timestamps: true },
 );
 
-// Creating the Student model with proper type
-const Student = mongoose.model<IStudent>('User', StudentSchema);
 
-export default Student;
+interface StudentInterface {
+    studentCode: string;
+    subRole: string;
+    enrolledCourses: mongoose.Types.ObjectId[];
+    currentCourse: mongoose.Types.ObjectId[]; 
+    bookedSessions: mongoose.Types.ObjectId[]; 
+    skills: string[];
+    educationDetails:[{}];
+    pastExperience:[{}]; 
+  }
+
+
+  const Student = mongoose.model<StudentInterface>('Student', StudentSchema);
+  
+  export default Student;
+  
+
+  
