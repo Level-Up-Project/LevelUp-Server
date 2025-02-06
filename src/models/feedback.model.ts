48ea6b7b3bc527509c376bb83a17d271850c14b4
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-02-06 17:46:28 +0530

    feedback.model.ts updated

--- src/models/feedback.model.ts
+++ src/models/feedback.model.ts
@@ -1,27 +1,40 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
-// Defining the interface for the Feedback document
 export interface IFeedback extends Document {
-  userId: mongoose.Types.ObjectId;
-  title: String,
+  title: String;
+  description: String;
   rating: number;
-  comment: string;
-  createdAt: Date;
+  givenBy: mongoose.Types.ObjectId;
 }
 
-// Define the schema for the Feedback model
 const FeedbackSchema: Schema = new Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    title: { type: String, required: true },
-    rating: { type: Number, required: true, min: 1, max: 5 }, // Rating between 1 and 5
-    comment: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+    title: {
+      type: String,
+      required: [true, 'Title is required'],
+    },
+
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+    },
+
+    rating: {
+      type: Number,
+      min: [1, 'Rating must be at least 1'],
+      max: [5, 'Rating cannot exceed 5'],
+      required: true,
+    },
+
+    givenBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'GivenBy (User ID) is required'],
+    },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
-// Creating the Feedback model with proper type
 const Feedback = mongoose.model<IFeedback>('Feedback', FeedbackSchema);
 
 export default Feedback;
