3bf042129f6f8b0ab8e9a35df0351003c313404d
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-02-08 01:54:50 +0530

    profileSetup.ts is added

--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { profileSetup, getStudentProfile } from '../controllers/v1/student/profileSetup';
+import { studentValidator } from '../validators/studentValidator';
+import { validateRequest } from '../middlewares/validateRequest';
 
 const studentRoute = express.Router();
 
-studentRoute.get('/profile-setup', (req, res) => {
-  res.send('Hello World!');
-});
+studentRoute.post('/setup', studentValidator, validateRequest, profileSetup);
+
+studentRoute.get('/:studentCode', getStudentProfile);
 
 export default studentRoute;
