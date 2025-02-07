91e875f1d8dfd45c9a49511e7e5f48225860d38c
Satyajeet Singh <satyajeet1152000@gmail.com>
2025-02-07 15:58:24 +0530

    Student routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,24 +1,22 @@
-import express from 'express'
-import register from '../controllers/v1/auth/register'
-import login from '../controllers/v1/auth/login'
-import registrationValidation from '../validators/registration-validators'
-import loginValidation from '../validators/loginValidator'
-import handleValidationErrors from '../validators/validationErrorHandlert'
-import authMiddleware from '../middlewares/auth.middleware'
-import logout from '../controllers/v1/auth/logout'
-import refreshAccessToken from '../controllers/v1/auth/refreshAccesstoken'
-import resetPassword from '../controllers/v1/auth/reset-password'
-import forgetPassword from '../controllers/v1/auth/forget-password'
+import express from 'express';
+import register from '../controllers/v1/auth/register';
+import login from '../controllers/v1/auth/login';
+import registrationValidation from '../validators/registration-validators';
+import loginValidation from '../validators/loginValidator';
+import handleValidationErrors from '../validators/validationErrorHandlert';
+import authMiddleware from '../middlewares/auth.middleware';
+import logout from '../controllers/v1/auth/logout';
+import refreshAccessToken from '../controllers/v1/auth/refreshAccesstoken';
+import resetPassword from '../controllers/v1/auth/reset-password';
+import forgetPassword from '../controllers/v1/auth/forget-password';
 
+const authRoute = express.Router();
 
-const router=express.Router()
+authRoute.post('/register', registrationValidation, handleValidationErrors, register);
+authRoute.post('/login', loginValidation, handleValidationErrors, login);
+authRoute.get('/logout', authMiddleware, logout);
+authRoute.get('/refreshAccessToken', refreshAccessToken);
+authRoute.post('/forget-password', forgetPassword);
+authRoute.post('/reset-password/:token', resetPassword);
 
-
-router.post('/register',registrationValidation, handleValidationErrors, register)
-router.post('/login',loginValidation, handleValidationErrors, login);
-router.get('/logout', authMiddleware, logout);
-router.get('/refreshAccessToken', refreshAccessToken);
-router.post('/forget-password', forgetPassword);
-router.post('/reset-password/:token', resetPassword);
-
-export default router
+export default authRoute;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
-import express from 'express'
-import userRouter from './auth.route';
-const router=express.Router()
+import express from 'express';
+import authRoute from './auth.route';
+import studentRoute from './student.route';
+const router = express.Router();
 
+router.use('/auth', authRoute);
+router.use('/student', studentRoute);
 
-router.use('/user', userRouter)
-
-
-export default router
+export default router;
