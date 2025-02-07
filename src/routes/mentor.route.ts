8abd0fb2ae3efdf6ab4f694e3ce0626efcd150a7
Satyajeet Singh <satyajeet1152000@gmail.com>
2025-02-07 22:49:20 +0530

    Mentor Routes Update | Swagger Implementation - inProgress

--- src/routes/mentor.route.ts
+++ src/routes/mentor.route.ts
@@ -2,17 +2,15 @@ import express from 'express';
 import { profileSetup } from '../controllers/v1/mentor/profile-setup';
 import profileSetupValidation from '../validators/mentor/profile-setup.validator';
 import handleValidationErrors from '../validators/validationErrorHandlert';
-import { getRequestedSessions } from '../controllers/v1/mentor/get-requested-sessions';
 import { sessionApprove } from '../controllers/v1/mentor/session-approve';
 import { getPastSession } from '../controllers/v1/mentor/get-past-session';
-import { getPresentSession } from '../controllers/v1/mentor/get-present-session';
+import { getSessions } from '../controllers/v1/mentor/get-sessions';
 
 const mentorRoute = express.Router();
 
 mentorRoute.post('/profile-setup', profileSetupValidation, handleValidationErrors, profileSetup);
-mentorRoute.get('/get-requested-sessions/:mentorId', getRequestedSessions);
+mentorRoute.get('/get-sessions/:mentorId', getSessions);
 mentorRoute.put('/session-approve', sessionApprove);
 mentorRoute.get('/get-past-session/:mentorId', getPastSession);
-mentorRoute.get('/get-present-session/:mentorId', getPresentSession);
 
 export default mentorRoute;
