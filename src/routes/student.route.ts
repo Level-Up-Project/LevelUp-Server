f252d2db530574bb4ad9fc5b8bc7fc47caf32a84
Eshita-10 <eshitasingh72@gmail.com>
2025-02-08 11:28:57 +0530

    student route

--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+// import sessionBooking from '../controllers/v1/student/session-booking';
+import authMiddleware from  '../middlewares/auth.middleware';
+import pastSession from '../controllers/v1/student/past-session';
+import upcomingSessions from '../controllers/v1/student/upcoming-session';
+
+
 
 const studentRoute = express.Router();
 
-studentRoute.get('/profile-setup', (req, res) => {
-  res.send('Hello World!');
-});
 
+studentRoute.get('/get-upcoming-sessions', authMiddleware, upcomingSessions);
+studentRoute.get('/get-past-sessions', authMiddleware, pastSession);
+
+// studentRoute.post('/session-booking', authMiddleware, sessionBooking);
 export default studentRoute;
