1ae4acba8761f95133e62a3aebf363811aa5b8c9
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-02-09 20:15:40 +0530

    role handeled in user-approval.ts

--- src/controllers/v1/admin/user-approval.ts
+++ src/controllers/v1/admin/user-approval.ts
@@ -5,14 +5,23 @@ import User from '../../../models/user.model.js';
 import ApiResponse from '../../../utils/ApiResponse.js';
 
 export const adminUserApproval = asyncHandler(async (req: Request, res: Response) => {
-    const { userId, status } = req.body;
+    const { userId, status, role } = req.body;
 
-    if (!userId || !status) {
+    if (!userId || !status || !role) {
         throw new ApiError(400, 'User ID and Status is required');
     }
 
     // Update the user status to approved
-    const user = await User.findByIdAndUpdate(userId, { $set: { status } });
+    const validRoles = ['user', 'student', 'mentor', 'admin', 'superAdmin'];
+    if (!validRoles.includes(role)) {
+        throw new ApiError(404, 'Invalid role specified');
+    }
+
+    const user = await User.findByIdAndUpdate(
+        userId, 
+        { $set: { status, role } }, 
+        { new: true, runValidators: true }
+    );
 
     if (!user) {
         throw new ApiError(400, 'User not found');
