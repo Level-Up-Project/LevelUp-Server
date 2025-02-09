30507505cb3f42075b628f1467d9b65828bac973
Satyajeet Singh <satyajeet1152000@gmail.com>
2025-02-09 22:33:55 +0530

    Schemas Update

--- src/controllers/v1/admin/user-approval.ts
+++ src/controllers/v1/admin/user-approval.ts
@@ -5,14 +5,14 @@ import User from '../../../models/user.model.js';
 import ApiResponse from '../../../utils/ApiResponse.js';
 
 export const adminUserApproval = asyncHandler(async (req: Request, res: Response) => {
-    const { userId, status } = req.body;
+    const { userId, status, role } = req.body;
 
     if (!userId || !status) {
         throw new ApiError(400, 'User ID and Status is required');
     }
 
     // Update the user status to approved
-    const user = await User.findByIdAndUpdate(userId, { $set: { status } });
+    const user = await User.findByIdAndUpdate(userId, { $set: { status, role } });
 
     if (!user) {
         throw new ApiError(400, 'User not found');
