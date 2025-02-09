501fe5c5a5175c0052813512771df66d557c5007
Eshita-10 <eshitasingh72@gmail.com>
2025-02-09 20:25:42 +0530

    course

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import authRoute from './auth.route.js';
 import studentRoute from './student.route.js';
 import mentorRoute from './mentor.route.js';
 import adminRoute from './admin.route.js';
+import courseRoute from './course.route.js';
 
 const router = express.Router();
 
@@ -10,5 +11,7 @@ router.use('/auth', authRoute);
 router.use('/student', studentRoute);
 router.use('/mentor', mentorRoute);
 router.use('/admin', adminRoute);
+router.use('/course', courseRoute);
+
 
 export default router;
